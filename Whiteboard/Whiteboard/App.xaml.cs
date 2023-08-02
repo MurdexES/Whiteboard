104a61372a9d7222b40013e0aa256391cdea4f4d
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-08-02 22:51:23 +0400

    Starting MVVM

--- Whiteboard/Whiteboard/App.xaml.cs
+++ Whiteboard/Whiteboard/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using SimpleInjector;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
+using Whiteboard.View;
+using Whiteboard.ViewModel;
 
 namespace Whiteboard
 {
@@ -13,5 +19,26 @@ namespace Whiteboard
     /// </summary>
     public partial class App : Application
     {
+        public static Container Container { get; set; } = new();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            MainStartup();
+        }
+
+        private void Register()
+        {
+            Container.RegisterSingleton<IMessenger, Messenger>();
+
+            Container.RegisterSingleton<MainViewModel>();
+        }
+
+        private void MainStartup()
+        {
+            var mainView = new MainView();
+            mainView.DataContext = Container.GetInstance<MainViewModel>();
+            mainView.ShowDialog();
+        }
     }
 }
